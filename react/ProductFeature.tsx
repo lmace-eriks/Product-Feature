2e1dce032b162f6e911619084ccff7fb4514495c
Web Developer <useraccount@WebDeveloperMBP.local>
2022-04-29 14:07:46 -0500

    Reset

--- react/ProductFeature.tsx
+++ react/ProductFeature.tsx
@@ -5,100 +5,13 @@ import { useState, useEffect, useRef, CSSProperties } from 'react';
 import styles from "./styles.css";
 
 interface ProductFeatureProps {
-  title: string,
-  image: string,
-  shopButtonLink: string,
-  moreButtonText: string,
-  moreButtonLink: string
   children: ReactChildren
 }
 
-const ProductFeature: StorefrontFunctionComponent<ProductFeatureProps> = ({ title, image, shopButtonLink, moreButtonText, moreButtonLink, children }) => {
-  const [openGate, setOpenGate] = useState<Boolean>(true);
-  const closeButtonText = "Close";
-  const classPrefix: string = "eriksbikeshop-productfeature-1-x-";
-  const [learnMoreButtonText, setLearnMoreButtonText] = useState<string>(moreButtonText || "Learn More");
-
-  const floatContainer = useRef<any>(!null);
-  const closeButton = useRef<any>(!null);
-  const bikeFeatureContainer = useRef<any>(!null);
-  const bikeSplit = useRef<any>(!null);
-  const textSplit = useRef<any>(!null);
-
-  useEffect(() => {
-    if (openGate) {
-      console.clear();
-      setOpenGate(false);
-    }
-  })
-
-  // VTEX adds a class prefix. This is a workaround - LM
-  const newClass = (classSuffix: string) => classPrefix + classSuffix;
-
-  const handleLearnMoreClick = () => {
-    if (learnMoreButtonText === moreButtonText) {
-      openInfo();
-    } else {
-      closeInfo();
-    }
-  }
-
-  const handleCloseButton = () => {
-    closeInfo();
-  }
-
-  const handleBackgroundClick = (e: any) => {
-    if (e.target.id === "floatContainer") {
-      if (learnMoreButtonText === closeButtonText) {
-        closeInfo();
-      }
-    }
-  }
-
-  const openInfo = () => {
-    bikeFeatureContainer.current.className = newClass("bikeFeatureContainerActive");
-    closeButton.current.className = newClass("closeButtonActive");
-    floatContainer.current.className = newClass("floatContainerActive");
-    textSplit.current.className = newClass("textSplitActive");
-    setLearnMoreButtonText(closeButtonText);
-  }
-
-  const closeInfo = () => {
-    bikeFeatureContainer.current.className = newClass("bikeFeatureContainer");
-    closeButton.current.className = newClass("closeButton");
-    floatContainer.current.className = newClass("floatContainer");
-    textSplit.current.className = newClass("textSplit");
-    setLearnMoreButtonText(moreButtonText);
-  }
+const ProductFeature: StorefrontFunctionComponent<ProductFeatureProps> = ({ children }) => {
 
   return (
-    <div>
-      <div id="floatContainer" ref={floatContainer} onClick={handleBackgroundClick} className={styles.floatContainer}>
-        <div ref={closeButton} onClick={handleCloseButton} className={styles.closeButton}>X</div>
-        <div className={styles.floatWrapper}>
-          <div ref={bikeFeatureContainer} className={styles.bikeFeatureContainer}>
-            <div className={styles.splitter}>
-              <div ref={bikeSplit} className={styles.bikeSplit}>
-                <h2 className={styles.bikeTitle}>{title}</h2>
-                <div className={styles.imageContainer}>
-                  <img className={styles.bikeImage} src={image} />
-                </div>
-                <div className={styles.buttonContainer}>
-                  <a href={shopButtonLink} className={styles.redButton}>Shop {title}</a>
-                  <div onClick={handleLearnMoreClick} className={styles.whiteButton}>{learnMoreButtonText}</div>
-                </div>
-              </div>
-              <div ref={textSplit} className={styles.textSplit}>
-                <div className={styles.descriptionWindow}>
-                  <p className={styles.innerDescriptionText}>{children}</p>
-                  <a href={moreButtonLink} target="_blank" rel="noreferrer" className={`${styles.whiteButton} ${styles.fullWidth}`}>{title} Buying Guide</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div>{children}</div>
   )
 }
 
@@ -107,49 +20,8 @@ ProductFeature.schema = {
   description: 'editor.productfeature.description',
   type: 'object',
   properties: {
-    title: {
-      title: "Title",
-      description: "Title of Feature",
-      type: "string"
-    },
 
-    // descriptionText: {
-    //   title: "Description Text",
-    //   description: "MD / Rich Text Not Supported",
-    //   type: "string"
-    // },
-
-    image: {
-      title: "Image Source",
-      description: "Full Path to Image",
-      type: "string"
-    },
-
-    shopButtonLink: {
-      title: "Shop Button Link",
-      description: "Full or Relative Path to Shop Link",
-      type: "string"
-    },
-
-    moreButtonText: {
-      title: "More Button Text",
-      description: "More Button Text",
-      type: "string",
-      default: "Learn More"
-    },
-
-    moreButtonLink: {
-      title: "More Button Link",
-      description: "Full or Relative URL Path",
-      type: "string"
-    }
   }
 }
 
 export default ProductFeature;
-
-
-// title: string,
-//   image: string,
-//   shopButtonLink: string,
-//   moreButton: MoreButton
